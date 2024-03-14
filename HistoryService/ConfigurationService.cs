3d3a4c8b128b996dde81f7be2366c3a837aad6d3
Renaldinho <rena0968@easv365.dk>
2024-03-14 17:30:02 +0100

    .

--- HistoryService/ConfigurationService.cs
+++ HistoryService/ConfigurationService.cs
@@ -4,6 +4,10 @@ using HistoryService.Application.Interfaces;
 using HistoryService.Infrastructure;
 using HistoryService.Infrastructure.Interfaces;
 using HistoryService.Infrastructure.Services;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
 
 namespace HistoryService;
 
@@ -14,10 +18,29 @@ public static class ConfigurationService
         services.AddScoped<IHistoryRepository, HistoryRepository>();
         services.AddScoped<IHistoryService, Application.Services.HistoryService>();
         services.AddDbContext<HistoryContext>();
-        
+
         var connectionString = Environment.GetEnvironmentVariable("EASYNETQ_CONNECTION_STRING");
         services.AddSingleton<IBus>(RabbitHutch.CreateBus(connectionString));
 
         services.AddHostedService<HistoryConsumer>();
+
+        services.AddOpenTelemetry()
+            .ConfigureResource(builder => builder.AddService("HistoryService"))
+            .WithTracing(builder =>
+                {
+                    builder
+                        .AddZipkinExporter(options =>
+                            options.Endpoint = new Uri("http://zipkin:9411/api/v2/spans"))
+                        .AddSource("HistoryService.HistoryConsumer")
+                        .SetSampler(new AlwaysOnSampler())
+                        .AddAspNetCoreInstrumentation()
+                        .AddConsoleExporter();
+                }
+            );
+        
+        // Log.Logger = new LoggerConfiguration()
+        //     .WriteTo.Seq("http://localhost:5341")
+        //     .CreateLogger();
+
     }
 }
